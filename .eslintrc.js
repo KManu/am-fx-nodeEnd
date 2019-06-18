6a8794696e54fc65d89468d50ddfa78eb18033ee
Manu <kmanu7@teamcyst.com>
2019-06-18 18:52:19 +0000

    Added pm2 ecosystem file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-    "extends": "google"
+    "extends": "google",
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "rules": {
+        "linebreak-style": 0,
+        "require-jsdoc": 0,
+        "no-tabs": 0,
+        "space-before-function-paren": 0,
+        "eol-last": 0,
+        "max-len": 0
+
+    }
 };
